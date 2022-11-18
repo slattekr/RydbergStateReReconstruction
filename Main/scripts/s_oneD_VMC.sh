9af692b3cf2418dbe2810add390e9c1bc7ed939a
Megan Schuyler Moss <msmoss@gra-login2.graham.sharcnet>
2022-11-18 10:19:54 -0500

    additional scripts for the cluster

---
+++ Main/scripts/s_oneD_VMC.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -t 5-00:00
+#SBATCH --mem=10000
+#SBATCH --account=rrg-rgmelko-ab
+#SBATCH --mail-user=msmoss@uwaterloo.ca
+#SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+module load python/3.7
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r requirements.txt
+
+python oneD_VMC.py
+
