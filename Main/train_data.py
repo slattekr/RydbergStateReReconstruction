94ac08bfaa22081ca72f7dd7c0378e4bef31bd1d
Megan Schuyler Moss <msmoss@gra-login1.graham.sharcnet>
2022-12-13 12:17:57 -0500

    get on the same page

--- Main/train_data.py
+++ Main/train_data.py
@@ -60,8 +60,8 @@ def Train_w_Data(config):
     if config['RNN'] == 'OneD':
         if config['Print'] ==True:
             print(f"Training a one-D RNN wave function with {num_hidden} hidden units and shared weights.")
-        # wavefxn = OneD_RNN_wavefxn(Lx,Ly,num_hidden,learning_rate,seed)
-        wavefxn = RNNWavefunction1D(Lx,Ly,num_hidden,learning_rate,seed)
+        wavefxn = OneD_RNN_wavefxn(Lx,Ly,num_hidden,learning_rate,seed)
+        # wavefxn = RNNWavefunction1D(Lx,Ly,num_hidden,learning_rate,seed)
     elif config['RNN'] =='TwoD':
         if config['Print'] ==True:
             print(f"Training a two-D RNN wave function with {num_hidden} hidden units and shared weights = {weight_sharing}.")
