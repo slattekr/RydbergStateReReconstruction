fbc7944e0da14d4720b56c4a11960892b0f9aa39
mschuylermoss <msmoss@uwaterloo.ca>
2022-12-21 12:00:45 -0500

    stag mag calculation script

---
+++ Main/stag_mag.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def generate_sublattices_square(Lx,Ly):
+    A_coords = []
+    B_coords = []
+    A_sites = []
+    B_sites = []
+    
+    for nx in range(Lx):
+        for ny in range(Ly):
+            if nx%2 == 0:
+                if ny%2 ==0:
+                    A_coords.append((nx,ny))
+                    A_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                else:
+                    B_coords.append((nx,ny))
+                    B_sites.append(coord_to_site_bravais(Lx,nx,ny))
+            else:
+                if ny%2 ==0:
+                    B_coords.append((nx,ny))
+                    B_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                else:
+                    A_coords.append((nx,ny))
+                    A_sites.append(coord_to_site_bravais(Lx,nx,ny))
+
+    return A_coords,B_coords,A_sites,B_sites
+
+
+def calculate_stag_mag(Lx,Ly,samples):
+    # generate sublattices
+    a,b,asites,bsites = generate_sublattices_square(Lx,Ly)
+    # do it for spin 1/2 units
+    samples = 0.5*(2*samples - 1)
+    # calculate sublattice A magnetization
+    magA = np.sum(samples[:,asites],axis=1)
+    # calculate sublattice B magnetization
+    magB = np.sum(samples[:,bsites],axis=1)
+    # calculate staggered magnetization
+    stag_mags = (-1*magA + magB)/(Lx*Ly)
+    avg_stag_mag = np.mean(stag_mags)
+    avg_stag_mag = np.mean(abs(stag_mags))
+    var = np.var(stag_mags)
+    return stag_mags,avg_stag_mag,var
+
