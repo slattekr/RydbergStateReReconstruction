bab2c4aa838dbb68e9a156a27d85c619334922d8
mschuylermoss <msmoss@uwaterloo.ca>
2023-01-17 08:53:02 -0500

    update stag mag calculation and results

--- Main/stag_mag.py
+++ Main/stag_mag.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def coord_to_site(Ly,x,y):
+    return Ly*x+y
+
 def generate_sublattices_square(Lx,Ly):
     A_coords = []
     B_coords = []
@@ -11,17 +14,17 @@ def generate_sublattices_square(Lx,Ly):
             if nx%2 == 0:
                 if ny%2 ==0:
                     A_coords.append((nx,ny))
-                    A_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                    A_sites.append(coord_to_site(Lx,nx,ny))
                 else:
                     B_coords.append((nx,ny))
-                    B_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                    B_sites.append(coord_to_site(Lx,nx,ny))
             else:
                 if ny%2 ==0:
                     B_coords.append((nx,ny))
-                    B_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                    B_sites.append(coord_to_site(Lx,nx,ny))
                 else:
                     A_coords.append((nx,ny))
-                    A_sites.append(coord_to_site_bravais(Lx,nx,ny))
+                    A_sites.append(coord_to_site(Lx,nx,ny))
 
     return A_coords,B_coords,A_sites,B_sites
 
@@ -38,7 +41,7 @@ def calculate_stag_mag(Lx,Ly,samples):
     # calculate staggered magnetization
     stag_mags = (-1*magA + magB)/(Lx*Ly)
     avg_stag_mag = np.mean(stag_mags)
-    avg_stag_mag = np.mean(abs(stag_mags))
+    abs_avg_stag_mag = np.mean(abs(stag_mags))
     var = np.var(stag_mags)
-    return stag_mags,avg_stag_mag,var
+    return stag_mags,avg_stag_mag,abs_avg_stag_mag,var
 
