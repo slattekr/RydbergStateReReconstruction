92118c621f0f9ef2f24b3f69b2ea57c497950f17
mschuylermoss <msmoss@uwaterloo.ca>
2023-01-17 14:13:32 -0500

    set up scripts for figure 2 runs !!!!

---
+++ Main/scripts/Figure_2/VMC_only_jobs.sh
@@ -0,0 +1,21 @@
+# #!/bin/bash
+
+# FSS Dataset
+
+joblist=$(sq -h --format="%j")
+
+# for delta in $(seq -1.545 0.5 13.455) # all deltas
+for delta in -1.545 4.455 4.955 13.455
+do
+    for seed in $(seq 100 100 100) # one seed for now
+    do
+        X="$delta|OneD|Nh=32|VMC|$seed"
+       	sbatch -J "$X" --export="delta=$delta,$dim='OneD',$nh=32,seed=$seed" submit_VMC_training.sh
+
+        X="$delta|TwoD|Nh=16|VMC|$seed"
+       	sbatch -J "$X" --export="delta=$delta,$dim='TwoD',$nh=16,seed=$seed" submit_VMC_training.sh
+            
+    done 
+    sleep 0.5s
+done
+
